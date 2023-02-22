77b1f0e9f4e580c8eff1aecec91f83bf11487ea9
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2023-02-23 01:27:26 +0800

    Initial commit

---
+++ src/InformationPage.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function InformationPage(){
+    return (
+        <div>
+            <p>FUCK kaijun</p>
+        </div>
+    )
+}
+
+export default InformationPage
