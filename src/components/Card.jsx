09c881753e4c4000af97f8e0faf3d66c77721793
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2023-02-26 04:59:25 +0800

    ???

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,9 +4,9 @@ const Card = ({ children }) => {
   return (
     <div className="h-[573px] w-[301px] bg-white rounded-t-lg text-black text-center font-[Inter]">
       <div>
-        <div className="font-[14px]"><b>Hello there!</b></div>
-        <div className="font-[10px]">
-          <p>Thank you for scanning my QR code!</p>
+        <div className="text-[14px] font-Inter"><b>Hello there!</b></div>
+        <div className="text-[10px] leading-[16.94px]">
+          <p className="text-[10px]">Thank you for scanning my QR code!</p>
 
           <p>I am <b>Mr Winston Huang</b>, and I have a tendency of forgetting where I am going
             and getting lost.</p>
@@ -17,6 +17,8 @@ const Card = ({ children }) => {
           <p>If it's too much trouble, you could also direct me to the nearest police station where I could obtain help.</p>
 
           <p>Thank you very much!</p>
+
+          <img src="src\assets\cool.jpg"></img>
         </div>
       </div>
       {children}
