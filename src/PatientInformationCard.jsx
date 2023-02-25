45a419bf39ec05a929c9249b546344c247ea2c5c
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2023-02-26 00:44:42 +0800

    lol

--- src/PatientInformationCard.jsx
+++ src/PatientInformationCard.jsx
@@ -3,40 +3,26 @@ function PatientInformationCard(){
     return(
         <div>
             <div className="flex">
-                <p className="font-bold">Elderly Patient Details</p>
-                <div className='bg-black rounded-3xl h-[40px] w-[40px] flex items-center justify-center' >
-                    <EditOutlinedIcon sx={{ color: "white" }} fontSize='large'/>
+                <p>English | 华语 | Melayu | தமிழ்</p>
+            </div>
+            <div className='Whitespace'>
+                <p>Hello there!</p>
+                <p>Thank you for scanning my QR Code!</p>
+                <p>I am Mr Winston Huang, and I have a tendency of forgetting where I am going and getting lost.</p>
+                <p>It appears that my caregiver is looking for me. I would really appreciate it if you could give Ms Lynn Lee a call (contact number below) and direct me back to my home (address below).</p>
+                <p>If it’s too much trouble, you could also direct me to the nearest police station where I could obtain help.</p>
+                <p>Thank you very much!</p>
+                <div className='flex'>
+                    <div>
+                        <img src= "https://thumbs.dreamstime.com/b/positice-senior-drinking-coffee-home-old-man-177914567.jpg" className='rounded-[113px] h-[171px] w-[171px]'/>
+                        <p>Mr Winston Huang</p>
+                        <p>Address</p>
+                        <p>Some gay address</p>
+                        <p>Postal Code</p>
+                    </div>
+                    <img src= "https://thumbs.dreamstime.com/b/positice-senior-drinking-coffee-home-old-man-177914567.jpg" className='rounded-[113px] h-[171px] w-[171px]'/>
                 </div>
             </div>
-            <img src= "https://thumbs.dreamstime.com/b/positice-senior-drinking-coffee-home-old-man-177914567.jpg" className='rounded-[113px] h-[171px] w-[171px]'/>
-            <div className='information'>
-                <div className='flex'>
-                    <div className="name">
-                        <p>Name</p>
-                        <p>Mr Wilson Huang</p>
-                    </div>
-                    <div className="age">
-                        <p>Age</p>
-                        <p>74</p>
-                    </div>
-                </div>
-                <div className='address'>
-                    <p>Address</p>
-                    <p>Something blablablabalbalbalbal, balbalblal</p>
-                </div>
-                <div className='postal-code'>
-                    <p>Postal Code</p>
-                    <p>Singapore 640623</p>
-                </div>
-                <div className='languages'>
-                    <p>Languages</p>
-                    <p>Hokkien, Mandarian</p>
-                </div>
-                <div className='hobbies'>
-                    <p>Likes, Hobbies</p>
-                    <p>Smart contract, tiaowu</p>
-                </div>
-            </div>        
         </div>
     )
 }
